e541363addbb3c18608d439ec4130c526a19d33a
Haut-Stone <haut.1604.stone@gmail.com>
2017-03-25 23:36:59 +0800

    update

---
+++ 练习场.cpp
@@ -0,0 +1,63 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2017 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2017-03-23 20:04:15
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2017-03-23 20:17:27
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <vector>
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2//提示这个已经定义过了
+typedef int Status;
+typedef int Elemtype;
+using namespace std;
+
+const int N = 1000;
+
+#if 1
+
+//线性表
+typedef struct Node
+{
+    int a[N];
+    int length;
+}List;
+
+#endif
+
+void initList1(List &L);
+void initList2(List *L);
+
+int main(void)
+{
+    List SL;
+    initList1(SL);
+    printf("%d\n", SL.length);
+    initList2(&SL);
+    printf("%d\n", SL.length);
+    return 0;
+}
+
+void initList1(List &L)
+{
+    L.length = 1;
+}
+
+void initList2(List *L)
+{
+    L->length = 2;
+}
